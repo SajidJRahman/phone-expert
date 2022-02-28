6e5a0de79312a9569910a65b81ff7b7b03710816
Sajid Rahman <sajidjrahman@icloud.com>
2022-02-28 23:36:12 +0100

    added search result

--- JS/phone.js
+++ JS/phone.js
@@ -14,9 +14,29 @@ const searchPhone = () => {
     }
     else if (searchText !== '') {
         notifyEmpty.style.display = 'none';
+        // fetch & load data
         const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
         fetch(url)
             .then(response => response.json())
-            .then(data => console.log(data))
+            .then(data => displayPhone(data.data))
+    }
+}
+
+const displayPhone = phones => {
+    const searchResult = document.getElementById('search-results');
+    for (const phone of phones) {
+        const div = document.createElement('div');
+        // console.log(phone);
+        div.classList.add('col');
+        div.innerHTML = `
+            <div class="card card-style">
+                <img class="p-3" src="${phone.image}" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h5 class="card-title">${phone.phone_name}</h5>
+                    <button class="btn btn-outline-dark rounded-pill px-5">Details</button>
+                </div>
+            </div>
+        `;
+        searchResult.appendChild(div);
     }
 }
